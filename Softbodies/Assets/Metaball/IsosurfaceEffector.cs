f56bfe969175b14b4e51ab5315fe9cc661d2a5c3
Ideefixze <kaczooorqu@gmail.com>
2020-09-16 13:18:33 +0200

    Delete Metaball

--- Softbodies/Assets/Metaball/IsosurfaceEffector.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class IsosurfaceEffector
-{
-    public abstract ComputeBuffer Evaluate(Vector3Int volumeSize, ComputeBuffer points);
-}
