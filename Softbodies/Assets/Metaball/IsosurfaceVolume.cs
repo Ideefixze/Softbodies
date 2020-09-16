ab087f364b1136f29ac145aad3f9367dbfbe7d55
Ideefixze <kaczooorqu@gmail.com>
2020-09-16 13:16:19 +0200

    Ignore Marching Cubes

--- Softbodies/Assets/Metaball/IsosurfaceVolume.cs
+++ Softbodies/Assets/Metaball/IsosurfaceVolume.cs
@@ -46,12 +46,17 @@ public class IsosurfaceVolume : MonoBehaviour
     private List<IsosurfaceEffector> _effectors;
 
     //Buffers
-    GraphicsBuffer triangleBuffer;
-    ComputeBuffer pointsBuffer;
-    ComputeBuffer triCountBuffer;
+    private ComputeBuffer _triangleBuffer;
+    private ComputeBuffer _pointsBuffer;
+    private ComputeBuffer _triCountBuffer;
+    private ComputeBuffer _argsBuffer;
+
+    [SerializeField] private Material _drawBuffer;
 
     [SerializeField]
-    ComputeShader marchingShader;
+    ComputeShader _marchingShader;
+    [SerializeField]
+    ComputeShader _fixupArgsCount;
     public int GetVolume()
     {
         return _sizeX * _sizeY * _sizeZ;
@@ -83,9 +88,9 @@ public class IsosurfaceVolume : MonoBehaviour
         int maxTriangleCount = numVoxels * 5;
 
         ReleaseBuffers();
-        triangleBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Append, maxTriangleCount, sizeof(float) * 3 * 3);
-        pointsBuffer = new ComputeBuffer(numPoints, sizeof(float) * 4);
-        triCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
+        _triangleBuffer = new ComputeBuffer(maxTriangleCount, sizeof(float) * 3 * 3,ComputeBufferType.Append);
+        _pointsBuffer = new ComputeBuffer(numPoints, sizeof(float) * 4);
+        _triCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
 
         float[] data = new float[4 * numPoints];
         int k = 0;
@@ -97,11 +102,11 @@ public class IsosurfaceVolume : MonoBehaviour
             data[i+3] = _points[k].value;
             k++;
         }
-        pointsBuffer.SetData(data);
+        _pointsBuffer.SetData(data);
 
         foreach(IsosurfaceEffector ie in _effectors)
         {
-            ie.Evaluate(new Vector3Int(_sizeX, _sizeY, _sizeZ), pointsBuffer);
+            ie.Evaluate(new Vector3Int(_sizeX, _sizeY, _sizeZ), _pointsBuffer);
         }
 
         
@@ -109,11 +114,12 @@ public class IsosurfaceVolume : MonoBehaviour
 
     void ReleaseBuffers()
     {
-        if (triangleBuffer != null)
+        if (_triangleBuffer != null)
         {
-            triangleBuffer.Release();
-            pointsBuffer.Release();
-            triCountBuffer.Release();
+            _triangleBuffer.Release();
+            _pointsBuffer.Release();
+            _triCountBuffer.Release();
+            _argsBuffer.Release();
         }
     }
 
@@ -121,37 +127,67 @@ public class IsosurfaceVolume : MonoBehaviour
     {
         PrepareBuffers();
 
-        triangleBuffer.SetCounterValue(0);
-        marchingShader.SetBuffer(0, "points", pointsBuffer);
-        marchingShader.SetBuffer(0, "triangles", triangleBuffer);
-        marchingShader.SetInt("sizeX", _sizeX);
-        marchingShader.SetInt("sizeY", _sizeY);
-        marchingShader.SetInt("sizeZ", _sizeZ);
-        marchingShader.SetFloat("isoLevel", 0f);
+        _triangleBuffer.SetCounterValue(0);
+        _marchingShader.SetBuffer(0, "points", _pointsBuffer);
+        _marchingShader.SetBuffer(0, "triangles", _triangleBuffer);
+        _marchingShader.SetInt("sizeX", _sizeX);
+        _marchingShader.SetInt("sizeY", _sizeY);
+        _marchingShader.SetInt("sizeZ", _sizeZ);
+        _marchingShader.SetFloat("isoLevel", 0f);
 
         
-        marchingShader.Dispatch(0, _sizeX - 1, _sizeY - 1, _sizeZ - 1);
+        _marchingShader.Dispatch(0, _sizeX - 1, _sizeY - 1, _sizeZ - 1);
 
         //Get number of triangles in the triangle buffer
-        GraphicsBuffer.CopyCount(triangleBuffer, triCountBuffer, 0);
+        GraphicsBuffer.CopyCount(_triangleBuffer, _triCountBuffer, 0);
         int[] triCountArray = { 0 };
-        triCountBuffer.GetData(triCountArray);
+        _triCountBuffer.GetData(triCountArray);
         int numTris = triCountArray[0];
         Debug.Log(numTris);
 
-        ComputeBuffer argsBuffer = new ComputeBuffer(4, sizeof(int), ComputeBufferType.IndirectArguments);
-        int[] args = new int[] {3, 1, 1, 1 };
-        argsBuffer.SetData(args);
-        
-        //ComputeBuffer.CopyCount(triangleBuffer, argsBuffer, 0);
+        _argsBuffer = new ComputeBuffer(4, sizeof(int), ComputeBufferType.IndirectArguments);
+        int[] args = new int[] {0, 1, 0, 0 };
+        _argsBuffer.SetData(args);
 
-        Graphics.DrawProceduralIndirect(GetComponent<MeshRenderer>().material, new Bounds(gameObject.transform.position, new Vector3(_sizeX, _sizeY, _sizeZ) * _spaceBetweenPoints),MeshTopology.Points, triangleBuffer,argsBuffer);
+        // Copy generated count
+        ComputeBuffer.CopyCount(_triangleBuffer, _argsBuffer, 0);
+
+        // Invoke very simple args fixup as generated count was triangles, not verts 
+        _fixupArgsCount.SetBuffer(0, "DrawCallArgs", _argsBuffer);
+        _fixupArgsCount.Dispatch(0, 1, 1, 1);
+
+        // Draw mesh using indirect args buffer
+        _drawBuffer.SetPass(0);
+        _drawBuffer.SetBuffer("_Buffer", _triangleBuffer);
+
+        Graphics.DrawProceduralIndirect(_drawBuffer, new Bounds(transform.position, transform.lossyScale),
+                                        MeshTopology.Points, _argsBuffer);
 
         //argsBuffer.Dispose();
-        //Graphics.DrawProceduralIndirect(GraphicsBuffer, new Bounds(gameObject.transform.position, new Vector3(_sizeX, _sizeY, _sizeZ) * _spaceBetweenPoints), MeshTopology.Triangles, argsBuffer);
-        //Graphics.DrawProceduralIndirectNow(MeshTopology.Triangles,triangleBuffer,);
-        // Get triangle data from shader
-        //UnityEngine.Rendering.AsyncGPUReadback.Request(triCountBuffer, FinishMeshIndirect);
+
+        //---------------------
+        Triangle[] tris = new Triangle[numTris];
+        _triangleBuffer.GetData(tris, 0, 0, numTris);
+
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+
+        var vertices = new Vector3[numTris * 3];
+        var meshTriangles = new int[numTris * 3];
+
+        for (int i = 0; i < numTris; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                meshTriangles[i * 3 + j] = i * 3 + j;
+                vertices[i * 3 + j] = tris[i][j];
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.triangles = meshTriangles;
+
+        mesh.RecalculateNormals();
 
     }
 
@@ -161,16 +197,16 @@ public class IsosurfaceVolume : MonoBehaviour
         trisCount = request.GetData<int>();
         int numTris = trisCount[0];
 
-        //Graphics.DrawProceduralIndirect(GetComponent<MeshRenderer>().material, new Bounds(gameObject.transform.position, new Vector3(_sizeX, _sizeY, _sizeZ) * _spaceBetweenPoints), MeshTopology.Triangles, triangleBuffer,numTris);
+        //Graphics.DrawProceduralIndirect(GetComponent<MeshRenderer>().material, new Bounds(gameObject.transform.position, new Vector3(_sizeX, _sizeY, _sizeZ) * _spaceBetweenPoints), MeshTopology.Triangles, _triangleBuffer,numTris);
     }
 
     /*
     public void FinishMesh(UnityEngine.Rendering.AsyncGPUReadbackRequest request)
     {
-        // Get number of triangles in the triangle buffer
-        ComputeBuffer.CopyCount(triangleBuffer, triCountBuffer, 0);
+        //Get number of triangles in the triangle buffer
+        ComputeBuffer.CopyCount(_triangleBuffer, _triCountBuffer, 0);
         int[] triCountArray = { 0 };
-        triCountBuffer.GetData(triCountArray);
+        _triCountBuffer.GetData(triCountArray);
         int numTris = triCountArray[0];
 
         NativeArray<Triangle> tris = new NativeArray<Triangle>();
@@ -198,7 +234,7 @@ public class IsosurfaceVolume : MonoBehaviour
     }
     */
 
-    public void Start()
+        public void Start()
     {
         InitializeVolume(_defaultValue);
 
